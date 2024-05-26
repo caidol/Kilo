92ae3d47a554d9b6640a639e742e7a2379972989
Aidan Catchpool <acatchpool@protonmail.com>
2024-05-26 22:08:18 +0100

    Getting window size, creating buffers

--- kilo.c
+++ kilo.c
@@ -4,14 +4,49 @@
 #include <termios.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <ctype.h>
 #include <unistd.h>
 
+/* defines */
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /* data */ 
-struct termios orig_termios;
+/* global struct containing our editor state */ 
+struct editorConfig {
+    struct termios orig_termios; /* Original termios structure settings to default back to */
+    int screenrows;              /* Number of rows that we can show */ 
+    int screencols;              /* Number of cols that we can show */ 
+};
+
+struct editorConfig E;
+
+/* append buffer */ 
+struct abuf {
+    char *b;
+    int len;
+};
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *src, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], src, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
 
 /* terminal */
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -19,25 +54,24 @@ void die(const char *s) {
 void disableRawMode() {
     /* Set all terminal attributes back to its original termios struct 
     to revert all changes */
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
         die("tcsetattr");
     }
 }
 
 void enableRawMode() {
     /* Retrieve original terminal attributes */
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
         die("tcgetattr");
     }
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     /* modify the input flag to turn off software flow control with XOFF/XON */
     raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
     
     /* disable post-processing outputs for output flag */
     raw.c_oflag &= ~(OPOST);
-
     raw.c_cflag |= ~(CS8);
 
     /* Flip all bits with bitwise NOT operator and perform a bitwise AND. Has effect 
@@ -54,23 +88,103 @@ void enableRawMode() {
     }
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+/* input */ 
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1; 
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    
+    //printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    }
+    else {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+        return 0;
+    }
+}
+
+/* output */
+
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        abAppend(ab, "~", 1);
+        
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
+    }
+}
+
+void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+    
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
 /* init */
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
-
-    //char c;
-    //while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    initEditor();
+ 
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        }
-        else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
-    }
+        editorRefreshScreen();
+        editorProcessKeypress();
+    }   
 
     return 0;
 }
