201d28a35c11d2e041e5c7ac5c1bba5d5e9df988
Aidan Catchpool <acatchpool@protonmail.com>
2024-06-14 22:24:40 +0100

    allow for inserting ordinary characters

--- kilo.c
+++ kilo.c
@@ -257,6 +257,10 @@ void editorProcessKeypress() {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+
+        default:
+            editorInsertChar(c);
+            break;
     }
 }
 
@@ -348,6 +352,25 @@ void editorAppendRow(char *s, size_t len) {
     editorUpdateRow(&E.row[row_idx]);
 
     E.numrows++;    
+}
+
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++; 
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/* editor operations */
+
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
 }
 
 /* file i/o operations */ 
